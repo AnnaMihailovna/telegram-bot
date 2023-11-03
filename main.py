cd9fc2a99618ab22cf891cd299a0b2b77120b328
AnnaMihailovna <anna.mihailovma@gmail.com>
2023-11-03 11:12:20 +0300

    start, send_text

--- main.py
+++ main.py
@@ -1,17 +1,55 @@
 import requests
+import telebot
+from datetime import datetime
 from dotenv import dotenv_values
 
 
 
 config = dotenv_values(".env")
 
-TELEGRAM_TOKEN = config.get("TELEGRAM_TOKEN")
+token = config.get("TELEGRAM_TOKEN")
 
 def get_data():
     req = requests.get("https://yobit.net/api/3/ticker/btc_usd")
     response = req.json()
-    print(response)
+    sell_price = response["btc_usd"]["sell"]
+    print(f"{datetime.now().strftime('%Y-%m-%d %H:%M')}\nSell BTC price: {sell_price}")
+
+def telegram_bot(token):
+    bot = telebot.TeleBot(token)
+
+    @bot.message_handler(commands=["start"])
+    def start_message(message):
+        bot.send_message(message.chat.id, "Привет! Набери 'price', чтобы узнать цену биткоина")
+
+
+    @bot.message_handler(content_types=["text"])
+    def send_text(message):
+        if message.text.lower() == "price":
+            try:
+                req = requests.get("https://yobit.net/api/3/ticker/btc_usd")
+                response = req.json()
+                sell_price = response["btc_usd"]["sell"]
+                print(f"{datetime.now().strftime('%Y-%m-%d %H:%M')}\nSell BTC price: {sell_price}")
+                bot.send_message(
+                    message.chat.id,
+                    f"{datetime.now().strftime('%Y-%m-%d %H:%M')}\nSell BTC price: {sell_price}"
+                )
+            except Exception as ex:
+                print(ex)
+                bot.send_message(
+                    message.chat.id,
+                    "Упс...Что-то пошло не так."
+                )
+        else:
+            bot.send_message(
+                    message.chat.id,
+                    "Не понял...Проверь команду, плиз!"
+            )
+
+    bot.polling()
 
 
 if __name__ == "__main__":
-    get_data()
+    # get_data()
+    telegram_bot(token)
